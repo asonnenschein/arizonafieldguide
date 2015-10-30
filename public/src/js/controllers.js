4922877ab57cc4d1774d33a5b47facabcd402814
asonnenschein <adrian.sonnenschein@gmail.com>
2015-10-29 19:25:19 -0700

    styled markers

--- public/src/js/controllers.js
+++ public/src/js/controllers.js
@@ -35,22 +35,38 @@
 
   function MapController ($scope, $routeParams, leafletData) {
     angular.extend($scope, {
-        defaults: {
-            tileLayer: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-            maxZoom: 14,
-            path: {
-                weight: 10,
-                color: '#800000',
-                opacity: 1
-            }
-        }
-    });
-    angular.extend($scope.layers.overlays, {
-      images: {
-        data: $scope.selectedData
+      defaults: {
+          tileLayer: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+          maxZoom: 14,
+          path: {
+              weight: 10,
+              color: '#800000',
+              opacity: 1
+          }
+      },
+      geojson: {
+        data: $scope.selectedData,
+        pointToLayer: function (feature, latlng) {
+          var marker = new L.marker(latlng, {icon: L.icon({
+            iconUrl: '/assets/images/marker.png',
+            iconSize: [38, 95]
+          })});
+          marker.on('click', function () {
+            console.log(this.feature.properties);
+          })
+          return marker;
+        },
       }
     });
-    console.log($scope.geojson);
+    leafletData.getMap().then(function(map) {
+        var latlngs, coord, i;
+        latlngs = [];
+        for (i = 0; i < $scope.selectedData.features.length; i++) {
+          coord  = $scope.selectedData.features[i].geometry.coordinates;
+          latlngs.push(L.GeoJSON.coordsToLatLng(coord));
+        }
+        map.fitBounds(latlngs);
+    });
   }
 
   angular
